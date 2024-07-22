4a5218ac069ef0845e9f047765aa02d118ce4453
MHD-Ammar <62371028+MHD-Ammar@users.noreply.github.com>
2024-07-22 17:07:27 +0300

    add image to schedual page

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect #import flask
 import json #import json
 import datetime #import datetime
-
+# python -m flask --app app.py --debug run
 app = Flask(__name__, template_folder='templates')
 
 #use datetime to know the date of today
